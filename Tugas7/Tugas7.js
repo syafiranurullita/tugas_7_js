60fbc49887d779bea076c2c8ac29754db91dec51
syafiranurullita <syafirnurullita3107@gmail.com>
2022-07-18 18:58:19 +0700

    tugas 7 js
    untuk NIOMIC

---
+++ Tugas7/Tugas7.js
@@ -0,0 +1,14 @@
+var Tinggi_badan = [120,130,140,160,170,180,185,187,190,195];
+for(let x of Tinggi_badan){
+  console.log(x);
+}
+
+
+// perulangan biasa
+function perulangan() {
+  for (var i = 0; i < 20; i++) {
+    console.log([120,130,140,160,170,180,185,187,190,195])
+  }
+}
+
+perulangan()
